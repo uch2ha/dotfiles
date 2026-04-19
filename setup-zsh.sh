5375e8c566dbc1074e0b865d0d529a9cc890d98d
uch2ha <sinyavskiy.d@gmail.com>
2026-04-19 11:24:50 +0000

    fix eza alias

--- setup-zsh.sh
+++ setup-zsh.sh
@@ -237,7 +237,7 @@ install_eza() {
   sudo mkdir -p /etc/apt/keyrings
 
   wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc \
-    | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+    | sudo gpg --batch --yes --dearmor -o /etc/apt/keyrings/gierens.gpg
 
   echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" \
     | sudo tee /etc/apt/sources.list.d/gierens.list >/dev/null
