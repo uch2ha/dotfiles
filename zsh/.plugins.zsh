7de1d760a43616a4d8045a2af7c63758316cb413
uch2ha <sinyavskiy.d@gmail.com>
2026-04-19 10:24:02 +0000

    add .aliases and .plugins.zsh

---
+++ zsh/.plugins.zsh
@@ -0,0 +1,6 @@
+# List of external Zsh plugins to install
+# (built-in plugins like git, z are not listed here)
+ZSH_CUSTOM_PLUGINS=(
+  "zsh-users/zsh-syntax-highlighting"
+  "zsh-users/zsh-autosuggestions"
+}
